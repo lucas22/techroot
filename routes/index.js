2fbc0cc8d8bde2a31cfc38a2ca54550b419e57b9
lucasparza <lucasparza@gmail.com>
2016-04-08 15:12:30 -0400

    Home page static content

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TECH_ROOT' });
+  res.render('index', {app_name: 'techroot'});
 });
 
 module.exports = router;
