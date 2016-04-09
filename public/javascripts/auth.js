67ca70cb345395007b47137d26ab452fa332525a
kqueiroz <kqueiroz@nd.edu>
2016-04-08 21:48:04 -0400

    sign_up_form

--- public/javascripts/auth.js
+++ public/javascripts/auth.js
@@ -16,11 +16,23 @@ $(document).ready(function () {
     $("#link_signup").click(function (e) {
         e.preventDefault();
         // TODO: create signup form fields
-        alert("Signup attempt!");
+        $("#link_signup").hide();
+        $("#btn_login").hide();
+        $("#login_form").append("<input id='confirm_pass' type='password' placeholder='****' required/>" +
+            "<input id='signup_name' type='text' placeholder='Your name' required/>" +
+            "<input id='signup_nick' type='text' placeholder='nickname'/>" +
+            "<input type='radio' name='gender' value='male'/>Male" +
+            "<input type='radio' name='gender' value='female'/>Female" +
+            "<input type='radio' name='gender' value='other'/>Other" +
+            "<input id='signup_submit_but' type='submit'/>"
+        );
+
+
+        //alert("Signup attempt!");
         // TODO: create user in firebase
 
         // If successful, send user to profile page:
-        window.location.href = "/profile"
+        // window.location.href = "/profile"
     });
 
 });
