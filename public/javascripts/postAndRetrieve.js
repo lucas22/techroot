9bd18344593e88cfeb047de36511e2ec4fb09802
Lucas Parzianello <lucas22@users.noreply.github.com>
2016-04-25 16:08:07 -0400

    Deleted obsolete files; Firebase entry location fixed; New 'todo': fix firebase entry key for uploads.

--- public/javascripts/postAndRetrieve.js
+++ public/javascripts/postAndRetrieve.js
@@ -5,21 +5,18 @@
 var ref = new Firebase("https://techroot.firebaseio.com/");
 var provider = ref.getAuth().provider;
 var uid = ref.getAuth().uid;
-var ref2 = new Firebase("https://techroot.firebaseio.com/users/"+uid)
-var ref3 = new Firebase("https://techroot.firebaseio.com/images/");
-//console.log(ref.getAuth().provider)
+var ref_upl_img = new Firebase("https://techroot.firebaseio.com/uploads/image/");
+//var ref2 = new Firebase("https://techroot.firebaseio.com/users/"+uid);
 //problemas: qd provider é password, como recuperar o nome para uma variavel
 
-function verify(id){
-
+function verify(id) {
 }
 
-
 function retrieve(){
-    ref3.on("value", function(sanapshot){
-        console.log(snapshot.val())
-        for( var elem in snapshot.val()){
-            console.log("desespero"+elem)
+    ref_upl_img.on("value", function (sanapshot) {
+        console.log(snapshot.val());
+        for (var elem in snapshot.val()) {
+            console.log("desespero" + elem);
             var $node = $("<div class= 'Tile' >" +
                 "<div class= 'thumbnail'>" +
                 "<img class= 'profile_pic' src = '/images/silvio.png'>" +
@@ -30,18 +27,17 @@ function retrieve(){
                 " </div>");
             //console.log(snapshot.val()[elem].uid);
             var ref2 = new Firebase("https://techroot.firebaseio.com/users/"+snapshot.val()[elem].uid);
-            console.log("blablalba"+snapshot.val()[elem].uid)
+            console.log("blablalba" + snapshot.val()[elem].uid);
             ref2.on("value",function (snapshot2){
                 $node.children(".post_name").text(snapshot2.val().email);
                 console.log(snapshot2.val().name);
 
-            })
+            });
             $("#profile_wall").append($node);
         }
 
     })
 }
-
 
 function post(){
     var name = getName(provider);
@@ -54,10 +50,12 @@ function post(){
             likes: 0,
             timestamp: new Date().getTime()
         }
-    }
-    ref3.push(message);
-
+    };
+    ref_upl_img.push(message);
+    // TODO: the message entry key should be the same as file name:
+    // /public/uploads/random_file_name -> techroot/uploads/image/random_file_name/...
 }
+
 var getName =function (provider){
     var name;
     switch(provider){
@@ -67,28 +65,25 @@ var getName =function (provider){
         case "google":
             name = ref.getAuth().google.displayName;
             break;
-
         case "github":
             name = ref.getAuth().github.username;
             break;
     }
     return name;
-}
-
+};
 
 //console.log(getName(provider))
 $(document).ready(function(){
-
     //console.log($("#get_name").valueOf())
-    var x = getName(provider)
+    var x = getName(provider);
     //console.log(x)
 
     $("#submit_post").on("click", post);
 
-    ref3.on("value", function(snapshot){
-        console.log(snapshot.val())
+    ref_upl_img.on("value", function (snapshot) {
+        console.log(snapshot.val());
         for( var elem in snapshot.val()){
-            console.log("desespero"+elem)
+            console.log("desespero" + elem);
             var $node = $("<div class= 'Tile' >" +
                 "<div class= 'thumbnail'>" +
                 
@@ -98,14 +93,13 @@ $(document).ready(function(){
                 " </div>");
             //console.log(snapshot.val()[elem].uid);
             var ref2 = new Firebase("https://techroot.firebaseio.com/users/"+snapshot.val()[elem].uid);
-            console.log("blablalba"+snapshot.val()[elem].uid)
+            console.log("blablalba" + snapshot.val()[elem].uid);
             ref2.on("value",function (snapshot2){
                 $node.children(".post_name").text(snapshot2.val().email);
                 console.log(snapshot2.val().name);
-
-            })
+            });
             $("#profile_wall").append($node);
         }
 
     })
-})
+});
